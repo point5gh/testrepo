b84a309d006951eac6909b38b5bae99f9930bf66
point5gh <jallen22@cfl.rr.com>
2014-02-21 20:00:32 -0500

    first comment

---
+++ testwebapp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testwebapp.Startup))]
+namespace testwebapp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
